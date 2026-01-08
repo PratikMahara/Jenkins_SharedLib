cab194f80e29d5a052700b573c2143c90824e3d6
Pratik Mahara <maharapratik5@gmail.com>
2026-01-08 17:36:37 +0530

    Refactor sonar scanner tool initialization

--- vars/sonarqube_analysis.groovy
+++ vars/sonarqube_analysis.groovy
@@ -1,13 +1,15 @@
 def call(String sonarServer, String projectName, String projectKey) {
 
-    def scannerHome = tool(name: 'Sonar', type: 'hudson.plugins.sonar.SonarRunnerInstallation')
+    def scannerHome = tool(
+        name: 'Sonar',
+        type: 'hudson.plugins.sonar.SonarRunnerInstallation'
+    )
 
     withSonarQubeEnv(sonarServer) {
         sh """
             ${scannerHome}/bin/sonar-scanner \
             -Dsonar.projectName=${projectName} \
-            -Dsonar.projectKey=${projectKey} \
-            -X
+            -Dsonar.projectKey=${projectKey}
         """
     }
 }
